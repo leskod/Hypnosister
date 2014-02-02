a06158f22281444b34070a49a03f89c5920340aa
Dereck Lesko <lesko@moravian.edu>
2014-02-02 12:57:50 -0500

    first commit

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Lesko, Dereck on 2/2/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : NSObject
+
+@end
---
+++ Hypnosister/HypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Lesko, Dereck on 2/2/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+@end
