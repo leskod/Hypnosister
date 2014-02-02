ccaa59dc425d50744221d99be976e58566cad158
Dereck Lesko <lesko@moravian.edu>
2014-02-02 10:16:08 -0500

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Lesko, Dereck on 2/2/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
